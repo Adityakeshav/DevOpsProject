dc1cc98c0cc353ec4f172da68a75cb24e396ce88
Aditya Keshav <adityakeshav393@gmail.com>
2025-01-26 00:52:56 +0530

    change

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, this is my DevOps project where I am allocating and provisioning my resources via Terraform on Azure, and building my Flask application into a Docker image and pushing it to Docker Hub. For CI/CD, I am using GitHub Actions.'
+    return 'Hello , this is my DevOps project where I am allocating and provisioning my resources via Terraform on Azure, and building my Flask application into a Docker image and pushing it to Docker Hub. For CI/CD, I am using GitHub Actions.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
